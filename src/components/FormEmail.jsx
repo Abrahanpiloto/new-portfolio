53f405138992ed6a6540539327c389b4c14c14d4
Abrahan <abrahanpiloto@gmail.com>
2025-07-24 19:33:46 -0500

    email form arreglos

--- src/components/FormEmail.jsx
+++ src/components/FormEmail.jsx
@@ -1,8 +1,10 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import { Link, useNavigate } from "react-router-dom";
 
 const FormEmail = () => {
   const form = useRef();
+  const navigate = useNavigate();
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -29,8 +31,21 @@ const FormEmail = () => {
   return (
     <section className="min-h-screen bg-yellow-50 dark:bg-neutral-900 px-4 py-12 text-neutral-900 dark:text-neutral-100 flex items-center justify-center">
       <div className="w-full max-w-xl bg-white dark:bg-neutral-800 p-8 rounded-xl shadow-lg transition-colors duration-500 border border-gray">
-        <h1 className="text-3xl font-bold mb-6 text-center">Contáctame</h1>
-        <form ref={form} onSubmit={sendEmail} className="space-y-6">
+        {/* Botón de cerrar */}
+
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Escríbeme a mi em@il
+        </h1>
+
+        <form ref={form} onSubmit={sendEmail} className="realative space-y-6">
+          <button
+            onClick={() => navigate("/contacto")}
+            className="absolute top-4 right-4 w-6 h-6 rounded-full bg-neutral-200 hover:bg-neutral-300 dark:bg-gray-400 dark:hover:bg-neutral-500 flex items-center justify-center transition duration-300 cursor-pointer"
+          >
+            <span className="text-black dark:text-white text-sm font-bold ">
+              X
+            </span>
+          </button>
           <div>
             <label htmlFor="from_name" className="block font-semibold mb-1">
               Tu nombre
