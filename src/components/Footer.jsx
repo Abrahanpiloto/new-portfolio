9ad161c9269586bfd540128aca645a233eab8bde
Abrahan <abrahanpiloto@gmail.com>
2025-09-23 12:28:29 -0500

    se modifico el footer a sticky

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#FEFCE8] dark:bg-neutral-950 text-neutral-800 dark:text-neutral-200 py-2 px-4">
+    <footer className="fixed bottom-0 left-0 right-0 z-50 bg-[#FEFCE8] dark:bg-neutral-950 text-neutral-800 dark:text-neutral-200 py-2 px-4">
       <div className="max-w-6xl mx-auto flex flex-col  items-center text-center ">
         <p className="mb-2 md:mb-0">
           Creado por{" "}
