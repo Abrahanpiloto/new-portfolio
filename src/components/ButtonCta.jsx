2faedc50871bd6047ff5f4dce8e7aa10e26c84a0
Abrahan <abrahanpiloto@gmail.com>
2025-11-01 17:26:44 -0500

    se creo mejoraron servicios y CardPrice

--- src/components/ButtonCta.jsx
+++ src/components/ButtonCta.jsx
@@ -7,11 +7,11 @@ const ButtonCta = () => {
       href="https://wa.me/51916058633?text=Hola%20Abrahan,%20quiero%20impulsar%20mi%20negocio%20"
       target="_blank"
       rel="noopener noreferrer"
-      className="text-gray-800 w-78 lg:w-102 mx-auto flex justify-center items-center gap-1 bg-green-500 dark:bg-green-500 py-2 rounded-sm shadow-md hover:shadow-xl transition hover:bg-blue-700 hover:text-white mb-8 dark:text-white border-1"
+      className="text-gray-800 w-78 lg:w-102 mx-auto flex justify-center items-center gap-1 bg-green-500 dark:bg-green-500 py-2 rounded-sm shadow-md hover:shadow-xl transition hover:bg-blue-700 hover:text-white mb-8 mt-8 dark:text-white border-1"
     >
       <FaWhatsapp className="text-3xl" />
 
-      <span>Hablemos de tu negocio</span>
+      <span className="font-bold">Hablemos de tu negocio</span>
     </a>
   );
 };
