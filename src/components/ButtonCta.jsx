40e73039fbb4ab42d9b0faa77148fda444ed0706
Abrahan <abrahanpiloto@gmail.com>
2025-11-04 17:25:38 -0500

    se integro GA4

--- src/components/ButtonCta.jsx
+++ src/components/ButtonCta.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
+import { trackEvent } from "../analytics";
 
 const ButtonCta = () => {
+  const handleClick = () => {
+    // Enviar evento a GA4
+    trackEvent({
+      action: "click_whatsapp", // Nombre del evento
+      category: "contact", // Categoría
+      label: "header_button", // Etiqueta (puedes personalizar según lugar)
+    });
+  };
   return (
     <a
       href="https://wa.me/51916058633?text=Hola%20Abrahan,%20quiero%20impulsar%20mi%20negocio%20"
       target="_blank"
       rel="noopener noreferrer"
+      onClick={handleClick}
       className="text-gray-800 w-78 lg:w-102 mx-auto flex justify-center items-center gap-1 bg-green-500 dark:bg-green-500 py-2 rounded-sm shadow-md hover:shadow-xl transition hover:bg-blue-700 hover:text-white mb-8 mt-8 dark:text-white border-1"
     >
       <FaWhatsapp className="text-3xl" />
