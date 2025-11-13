d14889a136d5d04787914ff65d84e3e81a34c8c4
Abrahan <abrahanpiloto@gmail.com>
2025-11-13 07:29:50 -0500

    se agrego rrss al footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { SiInstagram } from "react-icons/si";
+import { FaThreads } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 left-0 right-0 z-50 bg-[#FCF9F5] dark:bg-neutral-950 text-neutral-800 dark:text-neutral-200 py-2 px-4">
-      <div className="max-w-6xl mx-auto flex flex-col  items-center text-center ">
+      <div className="max-w-6xl mx-auto flex justify-center gap-4 items-center text-center ">
         <p className="mb-2 md:mb-0">
           Creado por{" "}
           <span
@@ -13,6 +15,26 @@ const Footer = () => {
             Abrahan Piloto
           </span>{" "}
         </p>
+        <div className="flex gap-4 ">
+          <a
+            href="https://www.instagram.com/abrahan.freelancer"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+            className="transition-colors"
+          >
+            <SiInstagram className="text-xl hover:text-blue-500 cursor-pointer" />
+          </a>
+          <a
+            href="https://www.threads.net/@abrahan.freelancer"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Threads"
+            className="transition-colors"
+          >
+            <FaThreads className="text-xl hover:text-blue-500 cursor-pointer" />
+          </a>
+        </div>
       </div>
     </footer>
   );
